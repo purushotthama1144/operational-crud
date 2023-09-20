92a326553407e460f7ba65b48cc9c6ce6da5beae
purushotthama1144 <bvpuru1990@gmail.com>
2023-09-20 14:59:27 +0530

    initial changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'role', loadChildren: () => import('./role/role.module').then(m => m.RoleModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
